7f9168bdaf1b8bcb30c2474f1d2fbd09a33e8085
sergy1207 <58848931+sergy1207@users.noreply.github.com>
2021-03-12 08:53:01 +0100

    character Movement V1

---
+++ ProyectoFinalCurso/Assets/CharacterMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public GameObject box;
+
+    public float speed = 6f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, vertical, 0f).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+
+
+
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
