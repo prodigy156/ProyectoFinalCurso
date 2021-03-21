ac210fb0da798eaf0e8ce86fe3e3a1dc52b4d0dd
sergy1207 <58848931+sergy1207@users.noreply.github.com>
2021-03-21 18:47:00 +0100

    Gravedad y salto implementados

--- ProyectoFinalCurso/Assets/CharacterMovement.cs
+++ ProyectoFinalCurso/Assets/CharacterMovement.cs
@@ -5,54 +5,99 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public Transform box;
 
+    //character public atributes
+    public Transform box;
     public float speed = 6f;
     public float turnSpeed = 1000f;
+    public float gravity = -9.81f;
 
+    //character private atributes
     private bool lookingRight = true;
-    float yRotation = 90;
+    private float yRotation = 90;
+
+    //Gravity - GroundCheck
+    //Public
+    public Transform groundCheck;
+    public float groundDistance = 0.1f;
+    public LayerMask groundMask;
+
+    //
+    public float jumpHeight = 3f;
 
 
-    private 
+    //Private
+    Vector3 velocity; //vertical downwards velocity
+    bool isGrounded;
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(groundCheck.position, new Vector3(0.95f, groundDistance, 0.1f));
+    }
+
+    private void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, vertical, 0f).normalized;
+        Vector3 direction = new Vector3(horizontal, 0f, 0f).normalized;
+
+        
 
         if(direction.magnitude >= 0.1f)
         {
 
-            if(horizontal < 0f)
+            if(horizontal < 0f)//I
+            {
+                
+                lookingRight = false;
+            }
+            else if(horizontal > 0f)//D
             {
                 lookingRight = true;
-            }
-            else if(horizontal > 0f)
-            {
-                lookingRight = false;
+                
             }
 
             controller.Move(direction * speed * Time.deltaTime);
         }
-
-        if(lookingRight && yRotation < 90)
+        //Jump
         {
-            yRotation += turnSpeed * Time.deltaTime;
-
-            if (yRotation > 90)
-                yRotation = 90;
-        }
-        else if (!lookingRight && yRotation > -90f)
-        {
-            yRotation -= turnSpeed * Time.deltaTime;
-
-            if (yRotation < -90f)
-                yRotation = -90f;
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+            }
         }
 
-        box.rotation = Quaternion.Euler(new Vector3(0f, yRotation, 0f));
+        //gravity
+        {
+            isGrounded = Physics.CheckBox(groundCheck.position, new Vector3(0.95f, groundDistance, 0.1f),  new Quaternion(0,0,0,0), groundMask);
+
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
+
+        //change sight direction (rotation)
+        {
+            if (lookingRight && yRotation < 90)
+            {
+                yRotation += turnSpeed * Time.deltaTime;
+
+                if (yRotation > 90)
+                    yRotation = 90;
+            }
+            else if (!lookingRight && yRotation > -90f)
+            {
+                yRotation -= turnSpeed * Time.deltaTime;
+
+                if (yRotation < -90f)
+                    yRotation = -90f;
+            }
+
+            box.rotation = Quaternion.Euler(new Vector3(0f, yRotation, 0f));
+        }
     }
 }
