50b5d46d20955425c29b8766bd80795de8b6d5f2
sergy1207 <58848931+sergy1207@users.noreply.github.com>
2021-05-07 00:10:28 +0200

    Añado Grid manager y editor manager

---
+++ ProyectoFinalCurso/Assets/LevelEditor/Scripts/EditorAsset.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorAsset : MonoBehaviour
+{
+    public int gridPosY, gridPosX;
+    public int sizeY, sizeX;
+    public bool twoP;
+    public bool sideA;
+
+    public enum AssetSize
+    {
+        oneByOne,
+        twoByOne,
+        twoByTwo,
+        twoByFour
+    }
+    public AssetSize size;
+
+}
