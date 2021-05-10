d7c04e817ca855b78c413b7dbc460f5908725830
sergy1207 <58848931+sergy1207@users.noreply.github.com>
2021-05-11 00:19:07 +0200

    El grid de los assets escala correctamente las celdas simpre que se cambia el tamaño de la pantalla

---
+++ ProyectoFinalCurso/Assets/LevelEditor/Scripts/UI_Manager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour
+{
+    public GameObject container;
+    float width;
+    // Update is called once per frame
+    void Update()
+    {
+        if(width != container.GetComponent<RectTransform>().rect.width)
+        {
+            width = container.GetComponent<RectTransform>().rect.width;
+            Debug.Log(width);
+            Vector2 newSize = new Vector2 (width / 2.5f, width / 2.5f);
+            Vector2 newSpacing = new Vector2 (newSize.x/8, newSize.x/8);
+            float newPadding = newSize.x/8;
+
+            container.GetComponent<GridLayoutGroup>().cellSize = newSize;
+
+            container.GetComponent<GridLayoutGroup>().spacing = newSpacing;
+
+            container.GetComponent<GridLayoutGroup>().padding.bottom = (int)newPadding;
+            container.GetComponent<GridLayoutGroup>().padding.top = (int)newPadding;
+            container.GetComponent<GridLayoutGroup>().padding.right = (int)newPadding;
+            container.GetComponent<GridLayoutGroup>().padding.left = (int)newPadding;
+        }
+    }
+}
