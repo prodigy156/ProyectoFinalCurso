ffec3e2df4f8340ab9eb59dddee11dd38fe2d3be
ImLuker <47191994+ImLuker@users.noreply.github.com>
2021-05-18 20:47:05 +0200

    Añadidos archivos faltantes

---
+++ ProyectoFinalCurso/Assets/LevelEditor/Scripts/GridSquare.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSquare : MonoBehaviour
+{
+
+    public bool available = true;
+
+
+    /// <summary>
+    /// OnMouseDown is called when the user has pressed the mouse button while
+    /// over the GUIElement or Collider.
+    /// </summary>
+    void OnMouseDown()
+    {
+        if(available && EditorManager.instance.canPlace)
+        {
+            EditorManager.instance.AssignAssetToGrid(this.gameObject);
+        }
+    }
+}
